5eb2822914a3011e58be39c71678f60288c24f97
smolinks <smolinks.com@hotmail.com>
2019-08-18 14:18:56 +0300

    first commit

---
+++ pages/about.js
@@ -0,0 +1,23 @@
+/**
+ * smolynx2019@hotmail.com
+ */
+
+import Layout from '../components/Layout';
+
+const About = () => (
+    <Layout>
+        <div className="myTitle pt-4">
+            <h1>About</h1>
+            <p >Welcome to About page!</p>
+            <style jsx>{
+                `
+                .myTitle {                
+                    display:flex;
+                    flex-Direction:column;
+                }
+            `
+            }</style>
+        </div>
+    </Layout>);
+
+export default About;
